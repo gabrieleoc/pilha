1e6f6edc3a95e2b8bb801b9ac278dd35c339986b
GABRIELE OLIVEIRA COSTA <116209292+gabrieleoc@users.noreply.github.com>
2024-03-13 20:30:14 -0300

    Add files via upload

---
+++ pilha.py
@@ -0,0 +1,23 @@
+class Pilha:
+    def __init__(self):
+        self.itens = []
+
+    def esta_vazia(self):
+        return not bool(self.itens)
+
+    def empilhar(self, dado):
+        self.itens.append(dado)
+
+    def desempilhar(self):
+        if not self.esta_vazia():
+            return self.itens.pop()
+
+# Exemplo de uso
+pilha = Pilha()
+pilha.empilhar('A')
+pilha.empilhar('B')
+pilha.empilhar('C')
+
+print(pilha.desempilhar())  
+print(pilha.desempilhar())  
+print(pilha.desempilhar())  
